50c1928958417922ef2a7b82dd6381f58a1db272
LiDaHai <278118698@qq.com>
2020-02-29 22:17:50 +0800

    1

---
+++ LDHTestSDK/Classes/LDHSDKManager.h
@@ -0,0 +1,16 @@
+//
+//  LDHSDKManager.h
+//  LDHTestSDK
+//
+//  Created by LiDaHai on 2020/2/29.
+//
+
+#import <Foundation/Foundation.h>
+
+NS_ASSUME_NONNULL_BEGIN
+
+@interface LDHSDKManager : NSObject
+
+@end
+
+NS_ASSUME_NONNULL_END
---
+++ LDHTestSDK/Classes/LDHSDKManager.m
@@ -0,0 +1,12 @@
+//
+//  LDHSDKManager.m
+//  LDHTestSDK
+//
+//  Created by LiDaHai on 2020/2/29.
+//
+
+#import "LDHSDKManager.h"
+
+@implementation LDHSDKManager
+
+@end
